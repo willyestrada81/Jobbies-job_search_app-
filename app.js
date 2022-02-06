f3e289a6889169395336ac301a9f93133a6f5c14
William Estrada <william.estrada@ninjarmm.com>
2022-02-06 13:22:15 -0500

    updated version

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.get('/', (req, res) => res.render('landing'));
 //Jobs Route
 app.use('/', require('./routes/jobs'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT , console.log(`Server running on port ${PORT}`)) 
